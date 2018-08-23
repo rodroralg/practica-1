2233dc8e18d66fc887e05738430227ef26c6bdaa
ALUMNO-N415 <ALUMNO-N415@UCSP.AULAB>
2018-08-23 09:49:07 -0500

    trabajo 23/08/2018

--- trab1/main.cpp
+++ trab1/main.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a;
+    cin >> a;
+    int b=12;
+    int z=a+b;
+    cout << b << endl << z << endl;
+    cout << a+b << "=" << a << "+" << b << endl;
+    cout << "Hello world!" << endl << "hola" << endl << "Rodrigo" << endl;
+    //promedio notas
+    cout << "ingresa tus notas:" << endl;
+    int nota1;
+    int nota2;
+    int nota3;
+    cin >> nota1;
+    cin >> nota2;
+    cin >> nota3;
+    cout << "tu nota pronedio es: " << (nota1+nota2+nota3)/3 << endl;
+    //short int
+    short int p = 3766; //tamaño de 2 bytes= 16 beats
+    int q = p+1; // 4 bytes
+    cout << q << endl;
+    q=q+1;
+    cout << q << endl;
+    //int max
+    int t;
+    int x=12312312313;
+    cout << x << endl;
+    t = x - 572589575;
+    cout << t << endl;
     return 0;
 }
