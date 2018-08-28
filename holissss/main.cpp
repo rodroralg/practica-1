939756bdcdd657b794241981140244a284b0fc59
rodroralg <42581103+rodroralg@users.noreply.github.com>
2018-08-28 11:31:58 -0500

    trab 2

---
+++ holissss/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello\n world!" << endl;
+    int number1=0;
+    int number2=0;
+    int number3=0;
+    cout <<"ingresa dos enteros para comparar:" << endl;
+    cin >> number1 >> number2 >> number3;
+    if (number1==number2)
+        cout << number1 << "==" << number2 << endl;
+
+    if (number1>number2) && (number1>number3) {
+        cout << "el mayor numero es:" << number1 << endl;
+    else if
+
+            /*if (number1>number3)
+                cout << "el mayor numero es:" << number1 << endl;
+            }
+            else{
+                cout << "el mayor numero es:" << number3 << endl;
+                }
+    if (number2>number3)
+*/
+
+    return 0;
+}
